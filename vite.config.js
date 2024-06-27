ce467cb5e96725ed2e92e517df623483966e50eb
minDuck <129139200%2B1800001142@users.noreply.github.com>
2024-06-27 11:06:42 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -10,10 +10,9 @@ export default defineConfig({
                 'resources/css/orderDetail.scss',
                 'resources/css/register.scss',
                 'resources/js/register.js',
-               'resources/css/slider-slick.scss', 
-               'resources/js/slider-slick.js',
-               'resources/css/home.scss', 
-               'resources/css/product-card.scss', 
+                'resources/css/slider-slick.scss',
+                'resources/js/slider-slick.js',
+                'resources/css/product-card.scss',
             ],
             refresh: true,
         }),
